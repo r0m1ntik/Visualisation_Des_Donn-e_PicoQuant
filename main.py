b2cda19b578b91987fb3700ee0b3468963d53ca9
Lalanne Loic <loic.lalanne@etud.univ-pau.fr>
2022-03-16 07:45:18 +0000

    mise a jour de la boucle ajout des option

--- main.py
+++ main.py
@@ -21,6 +21,7 @@ import sys
 import getopt
 import json
 import copy
+from math import *
 
 from pytictoc import TicToc
 
@@ -84,148 +85,100 @@ Data = {
     'img': []
 }
 
-# Types de balises
-tyEmpty8 = struct.unpack(">i", bytes.fromhex("FFFF0008"))[0]
-tyBool8 = struct.unpack(">i", bytes.fromhex("00000008"))[0]
-tyInt8 = struct.unpack(">i", bytes.fromhex("10000008"))[0]
-tyBitSet64 = struct.unpack(">i", bytes.fromhex("11000008"))[0]
-tyColor8 = struct.unpack(">i", bytes.fromhex("12000008"))[0]
-tyFloat8 = struct.unpack(">i", bytes.fromhex("20000008"))[0]
-tyTDateTime = struct.unpack(">i", bytes.fromhex("21000008"))[0]
-tyFloat8Array = struct.unpack(">i", bytes.fromhex("2001FFFF"))[0]
-tyAnsiString = struct.unpack(">i", bytes.fromhex("4001FFFF"))[0]
-tyWideString = struct.unpack(">i", bytes.fromhex("4002FFFF"))[0]
-tyBinaryBlob = struct.unpack(">i", bytes.fromhex("FFFFFFFF"))[0]
-
-# Types de record
-rtPicoHarpT3 = struct.unpack(">i", bytes.fromhex('00010303'))[0]
-
 # Declanchage du compteur de temps écoulé
 t.tic()
 
 # Variables global
-global recNum, inputfile, outputfile, oflcorrection, truensync, dlen, isT2, globRes, numRecords, TPP, debutlignetime, indentation, createjson
+global recNum, inputfile, outputfile, globRes, numRecords,  debutlignetime, X,Y
 
-if __name__ == "__main__":
-    argv = sys.argv[0:]
-    if len(sys.argv) < 3:
-        print("USAGE: python3 main.py -i data/pt3/SRV_2.pt3 -o ./data/json/SRV_2.json")
+def lectureentete():
+    global inputfile, outputfile, recNum, numRecords, debutlignetime,X,Y
+    
+    # Vérifiez si le fichier d'entrée est un fichier PTU valide
+    # Les chaînes Python n'ont pas de caractères NULL de fin, elles sont donc supprimées
+    Ident = inputfile.read(16).decode("utf-8").strip('\0')
+
+    # Vérification du format du fichier
+    if Ident != "PicoHarp 300":
+        print("ERREUR: Ce n'est pas un fichier PT3.")
+        inputfile.close()
         exit(0)
-    try:
-        opts, args = getopt.getopt(argv, "hij")
-    except getopt.GetoptError:
-        print(f'{sys.argv[0]} -i <inputfile> -o <outputfile>')
-        sys.exit(2)
 
-    for opt, arg in opts:
-        print("fgchgvjhklml")
-        if opt in ("-h", "--help"):
-            print('test.py -i <inputfile> -o <outputfile>')
-            sys.exit(2)
-        elif opt in ("-j", "--json"):
-            print('test.py -i <inputfile> -o <outputfile>')
-            createjson = arg
-        elif opt in ("-i", "--indent"):
-            indentation = True
+    # Récuperation des données (non utilisé dans le json)
+    version = inputfile.read(6).decode("utf-8").strip('\0')
+    CreatorName = inputfile.read(18).decode("utf-8").strip('\0')
+    Creatorversion = inputfile.read(12).decode("utf-8").strip('\0')
+    fileTime = inputfile.read(18).decode("utf-8").strip('\0')
+    linefeed = inputfile.read(2).decode("utf-8").strip('\0')
+    Comment = inputfile.read(256).decode("utf-8").strip('\0')
 
-# creation de json enable par defaut
-createjson = True
-# indentation disable par defaut
-indentation = False
+    # Affichage des données récuperées
+    print("Version: " + version + "\nCreateur: " +
+        CreatorName + "\nVersion: " + Creatorversion + "\nDate|Heure: " + fileTime + "\nSaut de ligne: " + linefeed + "\nCommentaire: " + Comment)
 
-# Lecture du 2eme argument (r : lecture - b : format binaire)
-inputfile = open(sys.argv[1], "rb")
+    NoC, BpR, RoutChan, NboBoards, ActCu = struct.unpack(
+        "iiiii", inputfile.read(20))
 
-# Les éléments suivants sont nécessaires pour la prise en charge des chaînes larges
-outputfile = io.open(sys.argv[2], "w+", encoding="utf-8")
+    MeasurmentMode, SubMode, RangeNO, Offset, AcquisitionTime = struct.unpack(
+        "iiiii", inputfile.read(20))
+    StopAt = struct.unpack("<i", inputfile.read(4))[0]
+    StopOnOvfl = struct.unpack("<i", inputfile.read(4))[0]
+    restart = struct.unpack("<i", inputfile.read(4))[0]
+    DisplayLinLog = struct.unpack("<i", inputfile.read(4))[0]
+    DisplayTimeAxisFrom = struct.unpack("<i", inputfile.read(4))[0]
+    DisplayTimeAxisTo = struct.unpack("<i", inputfile.read(4))[0]
+    DisplayCountAxisFrom = struct.unpack("<i", inputfile.read(4))[0]
+    DisplayCountAxisTo = struct.unpack("<i", inputfile.read(4))[0]
 
-# Vérifiez si le fichier d'entrée est un fichier PTU valide
-# Les chaînes Python n'ont pas de caractères NULL de fin, elles sont donc supprimées
-Ident = inputfile.read(16).decode("utf-8").strip('\0')
+    print("NoC : ", NoC)
+    print("BpR : %d" % BpR)
+    print("RoutChan : %d" % RoutChan)
+    print("NboBoards : %d" % NboBoards)
+    print("ActCu : %d" % ActCu)
+    print("MeasurmentMode : %d" % MeasurmentMode)
+    print("SubMode : %d" % SubMode)
+    print("RangeNO : %d" % RangeNO)
+    print("Offset : %d" % Offset)
+    print("AcquisitionTime : %d" % AcquisitionTime)
+    print("StopAt : %d" % StopAt)
+    print("StopOnOvfl : %d" % StopOnOvfl)
+    print("restart : %d" % restart)
+    print("DisplayLinLog : %d" % DisplayLinLog)
+    print("DisplayTimeAxisFrom : %d" % DisplayTimeAxisFrom)
+    print("DisplayTimeAxisTo : %d" % DisplayTimeAxisTo)
+    print("DisplayCountAxisFrom : %d" % DisplayCountAxisFrom)
+    print("DisplayCountAxisTo : %d" % DisplayCountAxisTo)
 
-# Vérification du format du fichier
-if Ident != "PicoHarp 300":
-    print("ERREUR: Ce n'est pas un fichier PT3.")
-    inputfile.close()
-    outputfile.close()
-    exit(0)
+    # debutlec block suivant
 
-# Récuperation des données (non utilisé dans le json)
-version = inputfile.read(6).decode("utf-8").strip('\0')
-CreatorName = inputfile.read(18).decode("utf-8").strip('\0')
-Creatorversion = inputfile.read(12).decode("utf-8").strip('\0')
-fileTime = inputfile.read(18).decode("utf-8").strip('\0')
-linefeed = inputfile.read(2).decode("utf-8").strip('\0')
-Comment = inputfile.read(256).decode("utf-8").strip('\0')
+    inputfile.read(184)
 
-# Affichage des données récuperées
-print("Version: " + version + "\nCreateur: " +
-      CreatorName + "\nVersion: " + Creatorversion + "\nDate|Heure: " + fileTime + "\nSaut de ligne: " + linefeed + "\nCommentaire: " + Comment)
+    Resol = struct.unpack("f", inputfile.read(4))[0]
+    print("Resol : ", Resol)
 
-NoC, BpR, RoutChan, NboBoards, ActCu = struct.unpack(
-    "iiiii", inputfile.read(20))
+    inputfile.read(132)
 
-MeasurmentMode, SubMode, RangeNO, Offset, AcquisitionTime = struct.unpack(
-    "iiiii", inputfile.read(20))
-StopAt = struct.unpack("<i", inputfile.read(4))[0]
-StopOnOvfl = struct.unpack("<i", inputfile.read(4))[0]
-restart = struct.unpack("<i", inputfile.read(4))[0]
-DisplayLinLog = struct.unpack("<i", inputfile.read(4))[0]
-DisplayTimeAxisFrom = struct.unpack("<i", inputfile.read(4))[0]
-DisplayTimeAxisTo = struct.unpack("<i", inputfile.read(4))[0]
-DisplayCountAxisFrom = struct.unpack("<i", inputfile.read(4))[0]
-DisplayCountAxisTo = struct.unpack("<i", inputfile.read(4))[0]
+    Record = struct.unpack("<i", inputfile.read(4))[0]
+    print("Record : ", Record)
+    unuse = struct.unpack("<i", inputfile.read(4))[0]
+    # inputfile.read(unuse*4)
+    inputfile.read(24)
+    X=struct.unpack("<i", inputfile.read(4))[0]
+    Y=struct.unpack("<i", inputfile.read(4))[0]
+    print(" x: %d  \n y: %d" %(X,Y))
+    inputfile.read(108)
 
-print("NoC : ", NoC)
-print("BpR : %d" % BpR)
-print("RoutChan : %d" % RoutChan)
-print("NboBoards : %d" % NboBoards)
-print("ActCu : %d" % ActCu)
-print("MeasurmentMode : %d" % MeasurmentMode)
-print("SubMode : %d" % SubMode)
-print("RangeNO : %d" % RangeNO)
-print("Offset : %d" % Offset)
-print("AcquisitionTime : %d" % AcquisitionTime)
-print("StopAt : %d" % StopAt)
-print("StopOnOvfl : %d" % StopOnOvfl)
-print("restart : %d" % restart)
-print("DisplayLinLog : %d" % DisplayLinLog)
-print("DisplayTimeAxisFrom : %d" % DisplayTimeAxisFrom)
-print("DisplayTimeAxisTo : %d" % DisplayTimeAxisTo)
-print("DisplayCountAxisFrom : %d" % DisplayCountAxisFrom)
-print("DisplayCountAxisTo : %d" % DisplayCountAxisTo)
+    # get important variables from headers
+    numRecords = Record
+    globRes = Resol
 
-# debutlec block suivant
-
-inputfile.read(184)
-
-Resol = struct.unpack("f", inputfile.read(4))[0]
-print("Resol : ", Resol)
-
-inputfile.read(132)
-
-Record = struct.unpack("<i", inputfile.read(4))[0]
-print("Record : ", Record)
-unuse = struct.unpack("<i", inputfile.read(4))[0]
-# inputfile.read(unuse*4)
-inputfile.read(32)
-# TPP = struct.unpack("<i", inputfile.read(4))[0] / 1e6
-# TPP = 1004
-TPP = 982.33
-inputfile.read(108)
-
-# get important variables from headers
-numRecords = Record
-globRes = Resol
-
-print("Écriture de {}%d{} enregistrements, cela peut prendre un certain temps...".format(
-    color.RED_HL, color.END) % numRecords)
+    print("Écriture de {}%d{} enregistrements, cela peut prendre un certain temps...".format(
+        color.RED_HL, color.END) % numRecords)
 
 
 def readPT3():
-    global inputfile, outputfile, recNum, oflcorrection, dlen, numRecords, debutlignetime
+    global inputfile, outputfile, recNum, numRecords, debutlignetime,name,X,Y
     global pt, lg, px, Data, img
-    debutimg = False
     debutligne = False
     oflcount = 0
     T3WRAPAROUND = 65536
@@ -233,7 +186,10 @@ def readPT3():
     numligne = 0
     numimg = 0
     i = 0
-    Data["file"] = sys.argv[1]
+    TABPHOTON =[]
+    Data["file"] = name
+    Data["X"] = X
+    Data["Y"] = Y
 
     for recNum in range(0, numRecords):
         # The data is stored in 32 bits that need to be divided into smaller
@@ -271,10 +227,25 @@ def readPT3():
 
                 if dtime == 2:
                     debutligne = False
+                    intervale = (truensync - debutlignetime) / X
+                    px["pt"] = []
+                    for i in range (X):
+                        numpixel += 1
+                        lg["px"].append(copy.copy(px))
+                        px["px"] = numpixel
+                    #print(numimg)
+                    #print(numligne)
+                    for i in TABPHOTON:
+                        npx = floor( (i["nsync"]-debutlignetime) /intervale)
+                        if npx>=X : # il arrive parfois que la valeur nsync = truesync de fin de ligne
+                            npx=X-1
+                        lg["px"][npx]["pt"].append(copy.copy(i))
+                    TABPHOTON=[]
+
                     lg["nlg"] = numligne
                     img["lg"].append(copy.copy(lg))
                     lg["px"] = []
-                    if numligne == 32:
+                    if numligne == Y:
                         img["nimg"] = numimg
                         Data["img"].append(copy.copy(img))
                         img["lg"] = []
@@ -282,44 +253,77 @@ def readPT3():
         else:
             if debutligne:
                 if channel == 0 or channel > 4:  # Should not occur
-                    print("Illegal Channel: #%1d %1u" % (dlen, channel))
-                while(truensync > (debutlignetime + (numpixel)*TPP)):
-                    # ecriture du pixel dans la ligne et changement de pixel
-                    numpixel += 1
-                    lg["px"].append(copy.copy(px))
-                    px["px"] = numpixel
-                    px["pt"] = []
-
-                # ecriture du photon dans le pixel
+                    print("Illegal Channel: # %1u" % ( channel))
+                # stockage du photon
                 pt["dtime"] = dtime
                 pt["nsync"] = truensync
-                px["pt"].append(copy.copy(pt))
+                TABPHOTON.append(copy.copy(pt))
                 pt.clear
 
-                dlen += 1
+
+
         if recNum % 100000 == 0:
             sys.stdout.write("\r{}La progression: %.1f%% -- %.5f seconds{}".format(color.GREEN, color.END) %
                              (float(recNum)*100/float(numRecords), t.tocvalue()))
             sys.stdout.flush()
 
 
-oflcorrection = 0
-dlen = 0
-print("PicoHarp T3 data")
-readPT3()
+
+def main(indentation = False):
+    global inputfile, outputfile, recNum, numRecords, debutlignetime
+
+    # Lecture du 2eme argument (r : lecture - b : format binaire)
+    inputfile = open(inputfile, "rb")
+    print("PicoHarp T3 data")
+
+
+    lectureentete()
+    readPT3()
+
+    inputfile.close()
+
+
 t.toc('\nLe temps de replissage de la bibliotheque est de', restart=createjson)
 
-# Generation du fichier json
-if (createjson):
-    print("\nDebut du remplissage fichier...")
-    if (indentation):
-        json.dump(Data, outputfile, indent=4)
-    else:
-        json.dump(Data, outputfile)
-    t.toc('Temps d\'ecriture en json est de')
+    # Generation du fichier json
+    if (outputfile != None):
+        outputfile = io.open(outputfile, "w+", encoding="utf-8")
+        print("\nDebut du remplissage fichier...")
+        if (indentation):
+            json.dump(Data, outputfile, indent=4)
+        else:
+            json.dump(Data, outputfile)
+        outputfile.close()
+        t.toc('Temps d\'ecriture en json est de')
 
-# nettoyage du tableau
-# Data.clear
+    
 
-inputfile.close()
-outputfile.close()
+if __name__ == "__main__":
+    global name
+    name = sys.argv[0]
+    argv = sys.argv[1:]
+    inputfile = None
+    outputfile = None
+    Indentation = False
+    try:
+        opts, args = getopt.getopt(argv, "hi:o:")
+    except getopt.GetoptError:
+        print(f'{sys.argv[0]} -i <inputfile> -o <outputfile>')
+        sys.exit(2)
+    for opt, arg in opts:
+
+        if opt in ("-h", "--help"):
+            print('test.py -i <inputfile> -o <outputfile>')
+            sys.exit(2)
+        elif opt in ("-i", "--input"):
+            print('test.py -i <inputfile> -o <outputfile>')
+            inputfile = arg
+        elif opt in ("-o", "--output"):
+            outputfile = arg
+        elif opt in ("-I", "--indent"):
+            Indentation = True
+    if (inputfile == None):
+            print('test.py -i <inputfile> -o <outputfile>')
+            sys.exit(2)
+    main(Indentation)
+

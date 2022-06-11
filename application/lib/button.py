ebafa6af04d126ebd5f04e6c5b968a50172ba3c6
Badanin Roman <r.badanin@etud.univ-pau.fr>
2022-06-11 12:45:41 +0200

    modif

--- application/lib/button.py
+++ application/lib/button.py
@@ -16,10 +16,11 @@ class MyButton():
 
     def do_create_botton(self, canvasG, canvas1, canvas2, canvas3):
         # Varible de zoom (current X1)
-        global canva2_x, canva2_y, canva2_img, canva2_cercle, max_x, max_y, img_pos_x, img_pos_y
+        global canva2_x, canva2_y, canva2_img, canva2_cercle1, canva2_cercle2, max_x, max_y, img_pos_x, img_pos_y
         global old_x, old_y, oldx, oldy, creation_cercle
         canva2_img = 0
-        canva2_cercle = 0
+        canva2_cercle1 = 0
+        canva2_cercle1 = 0
         # pour le cercle
         max_x = 50
         max_y = 50
@@ -173,73 +174,34 @@ class MyButton():
         CreateToolTip(self.button_8, text='Reduire la taille du cercle')
         ######################## FIN BOTTON 8 ########################
 
-        def movement(x, y):
-            global img_pos_x, img_pos_y
-            canvas2.scan_dragto(x, y, gain=1)
-            img_pos_x = x
-            img_pos_y = y
-            CustomFunction.warning_log(
-                self, f'movement avec la souris x={x} y={y}')
-
-        def change_size_cercle(self):
-            global max_x, max_y, canva2_cercle
-
         def activate_create_cercle(self):
             global creation_cercle
             creation_cercle = not creation_cercle
             CustomFunction.success_log(
                 self, f"La création du cercle est {('désactivé', 'activé')[creation_cercle]} !")
 
-        def move_cercle(x, y, update=False):
-            global canva2_cercle, canva2_img, old_x, old_y, img_pos_x, img_pos_y
-            # lorsque on fait un zoom, on garge le cercle au meme endroit
-            if (update):
-                if (not canva2_cercle):
-                    creeCercle(x-max_x, y-max_y, x+max_x, y+max_y)
-                    return
-                else:
-                    # supprimer l'element cercle de canvas2
-                    canvas2.delete(canva2_cercle)
-                    # on crée le cercle
-                    # canvas2.move(canva2_cercle, x-max_x, y-max_y)
-                    creeCercle(x-max_x, y-max_y, x+max_x, y+max_y)
-            # sinon on le place a la bonne place et on met a jour les coordonées
+        def creeCercle(canva2_x, canva2_y, max_x, max_y, n):
+            global canva2_cercle1, canva2_cercle2
+            if (n == 1):
+                canva2_cercle1 = canvas2.create_oval(
+                    canva2_x,
+                    canva2_y,
+                    max_x,
+                    max_y,
+                    width=2,
+                    # fill="green",
+                    outline="red"
+                )
             else:
-                if (canva2_cercle):
-                    CustomFunction.warning_log(self, "Cercle est deja crée")
-                    return
-                else:
-                    # supprimer l'element cercle de canvas2
-                    canvas2.delete(canva2_cercle)
-                    # on crée le cercle
-                    # canvas2.move(canva2_cercle, x-max_x, y-max_y)
-                    print(
-                        f'x:{x} - y:{y} ----- img_pos_x: {img_pos_x} - img_pos_y: {img_pos_y}')
-                    if (x != canva2_x):
-                        # print("different")
-                        # a faire
-                        # on annule
-                        img_pos_x = 0
-                    creeCercle(x-max_x, y-max_y, x+max_x, y+max_y)
-                    # affiche le coordonnée
-                    print(x, y)
-                old_x = x
-                old_y = y
-
-        def creeCercle(canva2_x, canva2_y, max_x, max_y):
-            global canva2_cercle, old_x, old_y
-            canva2_cercle = canvas2.create_oval(
-                canva2_x,
-                canva2_y,
-                max_x,
-                max_y,
-                width=2,
-                # fill="green",
-                outline="red"
-            )
-
-            old_x = canva2_x
-            old_y = canva2_y
+                canva2_cercle2 = canvas2.create_oval(
+                    canva2_x,
+                    canva2_y,
+                    max_x,
+                    max_y,
+                    width=2,
+                    # fill="green",
+                    outline="red"
+                )
 
             CustomFunction.success_log(
                 self, f"Le cercle a été crée à la position x={canva2_x} y={canva2_y}")
@@ -252,28 +214,28 @@ class MyButton():
                 # print(TAB_PIXEL)
                 v = max(itertools.chain.from_iterable(TAB_PIXEL))
                 v = 256 / v
-                x = 10
-                y = 10
+                x = 0
+                y = 0
                 for i in range(Convert.Data["Y"]):
                     for j in range(Convert.Data["X"]):
                         color = ceil(TAB_PIXEL[i][j] * v)
                         color = "#%02x%02x%02x" % (color, color, color)
                         creeCanvaImage(
                             self, x, y, color)
-                        x = x + 4
-                    x = 10
-                    y = y + 4
+                        x = x + 1
+                    x = 0
+                    y = y + 1
                 image_cree = True
 
         def creeCanvaImage(self, x, y, color):
             self.button_5.destroy()
             canvas2.create_rectangle(
-                x, y, x+4, y+4, width=0, fill=color)
+                x, y, x+1, y+1, width=0, fill=color)
 
         canvas2.bind('<ButtonPress-1>',
-                     lambda event: onclick(event.x, event.y))
+                     lambda event: generer_cercle(event.x, event.y))
 
-        def onclick(eventx, eventy):
+        def generer_cercle(eventx, eventy):
             global centre_x, centre_y, creation_cercle, couronne_cree, image_cree
             global cercle_1_x, cercle_1_y, rayon_1
             global cercle_2_x, cercle_2_y, rayon_2
@@ -285,39 +247,66 @@ class MyButton():
                 centre_x = eventx
                 centre_y = eventy
                 print("Position du centre: x:", eventx, "| y:", eventy)
+                ventry1X.set(centre_x)
+                ventry1Y.set(centre_y)
             else:
                 if (cercle_1_x == 0 or cercle_1_y == 0):
                     cercle_1_x = eventx
                     cercle_1_y = eventy
-                    rayon_1 = ceil(
-                        sqrt(pow(centre_x-eventx, 2)+pow(centre_y-eventy, 2)))
+                    rayon_1 = calcul_rayon(self, eventx, eventy)
+                    rayoncercle1.set(rayon_1)
                     creeCercle(centre_x-rayon_1, centre_y -
-                               rayon_1, centre_x+rayon_1, centre_y+rayon_1)
+                               rayon_1, centre_x+rayon_1, centre_y+rayon_1, 1)
                     print("Position du premier cercle: x:",
                           eventx, "| y:", eventy, "| rayon: ", rayon_1)
                 else:
                     cercle_2_x = eventx
                     cercle_2_y = eventy
-                    rayon_2 = ceil(
-                        sqrt(pow(centre_x-eventx, 2)+pow(centre_y-eventy, 2)))
+                    rayon_2 = calcul_rayon(self, eventx, eventy)
+                    rayoncercle2.set(rayon_2)
                     creeCercle(centre_x-rayon_2, centre_y -
-                               rayon_2, centre_x+rayon_2, centre_y+rayon_2)
+                               rayon_2, centre_x+rayon_2, centre_y+rayon_2, 2)
                     print("Position du deuxieme cercle: x:",
                           eventx, "| y:", eventy, "| rayon: ", rayon_2)
                     # On met a faut la création du cercle
                     creation_cercle = False
                     couronne_cree = True
                     print("La couronne a bien été crée.")
-                    reset_coordonnee()
+                    # reset_coordonnee(self)
 
         def reset_coordonnee(self):
-            global cercle_1_x, cercle_1_y, cercle_2_x, cercle_2_y, centre_x, centre_y
+            global cercle_1_x, cercle_1_y, cercle_2_x, cercle_2_y, centre_x, centre_y, rayon_1, rayon_2
             centre_x = 0
             centre_y = 0
             cercle_1_x = 0
             cercle_1_y = 0
             cercle_2_x = 0
             cercle_2_y = 0
+            rayon_1 = 0
+            rayon_2 = 0
+
+        def recharge_donnee(self):
+            global rayon_1, rayon_2, centre_x, centre_y, canva2_cercle1, canva2_cercle2, couronne_cree
+            centre_x = int(ventry1X.get())
+            centre_y = int(ventry1Y.get())
+            rayon_1 = int(rayoncercle1.get())
+            rayon_2 = int(rayoncercle2.get())
+            canvas2.delete(canva2_cercle1)
+            canvas2.delete(canva2_cercle2)
+            couronne_cree = False
+            cree_cercle_apartir_button(self)
+
+        def cree_cercle_apartir_button(self):
+            global rayon_1, rayon_2, cercle_2_x, cercle_2_y, centre_x, centre_y, canva2_cercle1, canva2_cercle
+            creeCercle(centre_x-rayon_1, centre_y - rayon_1,
+                       centre_x+rayon_1, centre_y+rayon_1, 1)
+            creeCercle(centre_x-rayon_2, centre_y - rayon_2,
+                       centre_x+rayon_2, centre_y+rayon_2, 2)
+
+        def calcul_rayon(self, x, y):
+            global centre_x, centre_y
+            if (centre_x != 0 and centre_y != 0):
+                return ceil(sqrt(pow(centre_x-x, 2)+pow(centre_y-y, 2)))
 
         # rectangle de droite
         title = Label(canvas1, text='Données du centre')
@@ -336,59 +325,33 @@ class MyButton():
         entry1Y = Entry(canvas1, bd=0, textvariable=ventry1Y)
         canvas1.create_window(120, 70, window=entry1Y)
 
-        title2 = Label(canvas1, text='Données du premier cercle')
+        title2 = Label(canvas1, text='Rayon du premier cercle')
         title2.config(font=('helvetica', 14))
         canvas1.create_window(110, 110, window=title2)
 
-        label1X = Label(canvas1, text="X:")
+        label1X = Label(canvas1, text="R:")
         canvas1.create_window(15, 140, window=label1X)
-        ventry2X = StringVar()
-        entry2X = Entry(canvas1, bd=0, textvariable=ventry2X)
+        rayoncercle1 = StringVar()
+        entry2X = Entry(canvas1, bd=0, textvariable=rayoncercle1)
         canvas1.create_window(120, 140, window=entry2X)
 
-        label1Y = Label(canvas1, text="Y:")
-        canvas1.create_window(15, 170, window=label1Y)
-        ventry2Y = StringVar()
-        entry2Y = Entry(canvas1, bd=0, textvariable=ventry2Y)
-        canvas1.create_window(120, 170, window=entry2Y)
-
-        title3 = Label(canvas1, text='Données du deuxieme cercle')
+        title3 = Label(canvas1, text='Rayon du deuxieme cercle')
         title3.config(font=('helvetica', 14))
-        canvas1.create_window(110, 210, window=title3)
+        canvas1.create_window(110, 180, window=title3)
 
-        label2X = Label(canvas1, text="X:")
-        canvas1.create_window(15, 240, window=label2X)
-        ventry3X = StringVar()
-        entry3X = Entry(canvas1, bd=0, textvariable=ventry3X)
-        canvas1.create_window(120, 240, window=entry3X)
-
-        label2Y = Label(canvas1, text="Y:")
-        canvas1.create_window(15, 270, window=label2Y)
-        ventry3Y = StringVar()
-        entry3Y = Entry(canvas1, bd=0, textvariable=ventry3Y)
-        canvas1.create_window(120, 270, window=entry3Y)
+        label2X = Label(canvas1, text="R:")
+        canvas1.create_window(15, 210, window=label2X)
+        rayoncercle2 = StringVar()
+        entry3X = Entry(canvas1, bd=0, textvariable=rayoncercle2)
+        canvas1.create_window(120, 210, window=entry3X)
 
         button1 = Button(text='Valider', bg='brown',
-                         font=('helvetica', 14, 'bold'))
-        canvas1.create_window(110, 310, window=button1)
+                         font=('helvetica', 14, 'bold'), command=lambda: recharge_donnee(self))
+        canvas1.create_window(110, 250, window=button1)
 
         # valeur par default
 
         ventry1X.set(centre_x)
         ventry1Y.set(centre_y)
-
-        ventry2X.set(cercle_1_x)
-        ventry2Y.set(cercle_1_y)
-
-        ventry3X.set(cercle_2_x)
-        ventry3Y.set(cercle_2_y)
-
-        # l = Label(canvas1, text="Centre - x")
-        # l.pack(side=LEFT)
-        # e = Entry(canvas1, bd=2)
-        # e.pack(side=RIGHT)
-
-        # d = ceil(sqrt(pow(x-j,2)+pow(y-i,2)))
-        # x, y coordonnées du centre
-        # j, i coordonnées du points cliqué pour le rayon
-        # d le rayon
+        rayoncercle1.set(cercle_1_x)
+        rayoncercle2.set(cercle_2_x)
